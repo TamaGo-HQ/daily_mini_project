361967e672ea7c4ae2667346a88a8ca2fba7ac4f
louka <melek.guedidi@insat.ucar.tn>
2025-08-20 22:12:55 +0100

    modified function headers and buffer struct to be thread_safe

--- circular_buffer/include/circular_buffer.h
+++ circular_buffer/include/circular_buffer.h
@@ -3,6 +3,16 @@
 
 #include <stdint.h> //for size_t
 #include <stdbool.h> //for bool
+
+// Windows mutex
+#ifdef _WIN32
+    #include <windows.h>
+    typedef CRITICAL_SECTION cb_mutex_t;
+#else
+    #include <pthread.h>
+    typedef pthread_mutex_t cb_mutex_t;
+#endif
+
 // Return codes for operations
 typedef enum{
     CB_SUCCESS = 0,
@@ -11,6 +21,8 @@ typedef enum{
     CB_ERROR_EMPTY,
     CB_ERROR_INVALID_SIZE,
     CB_ERROR_MEMORY_ALLOCATION,
+    CB_ERROR_MUTEX_INIT,
+    CB_ERROR_MUTEX_LOCK
 } cb_result_t;
 
 // Circular buffer structure
@@ -21,6 +33,7 @@ typedef struct{
     size_t head;         // Write position (producer index)
     size_t tail;         // Read poistion (consumer index)
     size_t count;        // Current number of elements
+    cb_mutex_t lock;        // Mutex for thread safety
 }circular_buffer_t;
 
 // Core API functions
